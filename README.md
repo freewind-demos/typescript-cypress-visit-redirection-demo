39b37aaf468989103a9b69a5315a4d3d8245b1b8
freewind <nowindlee@gmail.com>
2019-05-03 22:42:36 +0800

    demo

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
-TypeScript Cypress Test Demo
+TypeScript Cypress Visit "redirection" Demo
 =========================================
 
-注意点：
-
-1. `cypress/plugins/index.js`不能使用`.ts`，所以`webpack.config.js`也不能使用`.ts`
-2. 由于混用了`.js`与`.ts`，所以webpack中要设置`resolve`->`extensions`
-3. `tsconfig.json`中一定要设置`"types": [ "cypress" ]`，否则`cy`等typing找不到
+如果`cy.visit`访问的url有redirection, cypress会等待其正常结束。
 
 ```
 npm install
--- cypress/integration/hello_spec.ts
+++ cypress/integration/hello_spec.ts
@@ -1,39 +1,6 @@
-import {add} from '../../add'
-
-describe('TypeScript', () => {
-  it('works', () => {
-    // note TypeScript definition
-    const x: number = 42
+describe('cypress', () => {
+  it('wait for redirection', () => {
+    cy.visit('http://deelay.me/1000/https://example.cypress.io/')
+    cy.get('h1').should('have.text', 'Kitchen Sink');
   })
-
-  it('checks shape of an object', () => {
-    const object = {
-      age: 21,
-      name: 'Joe',
-    }
-    expect(object).to.have.all.keys('name', 'age')
-  })
-
-  it('uses cy commands', () => {
-    cy.wrap({}).should('deep.eq', {})
-  })
-
-  it('tests our example site', () => {
-    cy.visit('https://example.cypress.io/')
-    cy.get('.home-list')
-      .contains('Querying')
-      .click()
-    cy.get('#query-btn').should('contain', 'Button')
-  })
-
-  // enable once we release updated TypeScript definitions
-  it('has Cypress object type definition', () => {
-    expect(Cypress.version).to.be.a('string')
-  })
-
-
-  it('adds numbers', () => {
-    expect(add(2, 3)).to.equal(5)
-  })
-
 })
